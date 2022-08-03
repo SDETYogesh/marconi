98c72be823151c298b00a73de7c5fade166f76fb
yogeshwankhede <yogirajwankhede@gmail.com>
2022-08-03 15:27:00 +0530

    start the show

---
+++ src/test/java/com/stringdemo/practice/GitCommit.java
@@ -0,0 +1,16 @@
+//
+//package com.stringdemo.practice;
+//
+//public class GitCommit {
+//	System.out.println("Enter your message here=>  ");
+////	String input=scan.nextLine();
+////	Assertion soft = new Assertion();
+////	soft.assertEquals(input, "hello");
+////	if(input.equals("hello")) 
+////		Reporter.log("Tc pass");
+////	else
+////		Reporter.log("Tc fail");
+//
+//}
+//
+//}
